8fd07e1dd8b0b7e0065fa6883612ff213e57ecc9
amadeamargo <amadeamargo@gmail.com>
2022-02-19 23:15:58 -0600

    added routes and navlinks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { NavLink, Routes, Route } from 'react-router-dom'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faHouse, faFutbol, faNewspaper, faTemperatureHalf } from '@fortawesome/free-solid-svg-icons';
+import Home from './components/home/Home'
+import About from './components/about/About'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <NavLink to='/'> Home </NavLink>
+        <NavLink to='/about'> About </NavLink>
+        <NavLink to='/portfolio'> Portfolio </NavLink>
+        <NavLink to='/contact'> Contact </NavLink>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/portfolio' element={<Portfolio />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+      </Routes>
+    </>
   );
 }
 
